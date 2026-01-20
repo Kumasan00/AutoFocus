714d9c0d8258e5fbc0b93c37a7f7482ebce674f4
kuma <kumasan00m@gmail.com>
2026-01-21 00:28:19 +0900

    Initial Commit

---
+++ AutoFocus/AutoFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoFocusApp.swift
+//  AutoFocus
+//
+//  Created by 角谷拓武 on 2026/01/21.
+//
+
+import SwiftUI
+
+@main
+struct AutoFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
